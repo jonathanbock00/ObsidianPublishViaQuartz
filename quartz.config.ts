1912df2730e071d343d2cdfe374473cdc8359ed2
jonathanbock00 <jonathanbock00@gmail.com>
2024-09-27 10:44:14 +0200

    Quartz sync: Sep 27, 2024, 10:44 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "jonathanbock00.github.io/ObsidianPublishViaQuartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
